3c96078799395466596f9bc58548a3b1d1d5a1a7
Diego Alexander Ramirez Acosta <alexander221b@hotmail.com>
2022-03-23 09:37:04 -0500

    show message only once

--- lib.php
+++ lib.php
@@ -10,6 +10,42 @@
  */
 
  function local_message_before_footer(){
-    // Add a notification of some kind.
-   \core\notification::add('test message', \core\output\notification::NOTIFY_INFO);
+    global $DB, $USER;
+    
+    //Garantiza que los mensajes sólo se vean una vez
+    $sql = 'SELECT lm.id, lm.messagetext, lm.messagetype
+            FROM {local_message} lm
+            left outer join {local_message_read} lmr ON lm.id = lmr.messageid
+            WHERE lmr.userid <> :userid
+            OR lmr.userid IS NULL';
+
+    $params = [
+      'userid' => $USER->id 
+    ];
+
+    $messages = $DB->get_records_sql($sql, $params);
+
+    foreach($messages as $message){
+
+      $type = \core\output\notification::NOTIFY_INFO;
+
+      if($message->messagetype === '0'){
+        $type = \core\output\notification::NOTIFY_WARNING;
+      }
+      if($message->messagetype === '1'){
+        $type = \core\output\notification::NOTIFY_SUCCESS;
+      }
+      if($message->messagetype === '2'){
+        $type = \core\output\notification::NOTIFY_ERROR;
+      }
+
+      // Add a notification of some kind.
+      \core\notification::add($message->messagetext, $type);
+
+      $readrecord = new stdClass();
+      $readrecord->messageid = $message->id;
+      $readrecord->userid = $USER->id;
+      $readrecord->timeread = time();
+      $DB->insert_record('local_message_read', $readrecord);
+    }
  }
